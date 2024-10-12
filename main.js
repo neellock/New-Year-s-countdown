65bdb0470ed2444ff99dc95bf410eba6c24d6434
neellock <108747416+neellock@users.noreply.github.com>
2024-10-12 17:01:41 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,4 @@
-/*1. Créer une variable « newyear » et lui affecter la valeur de la date de la
-nouvelle année à venir. Celle-ci est une chaine de caractère et au format
-« j m aaaa », le mois est représenté par trois lettre (jan pour janvier par
-exemple).
-2. Créer une fonction « compteur ». Attention aux accolades.
-3. Nous allons avoir besoin de convertir la date de la nouvelle année dans un
-format numérique. Cela nous permettra de compter les jours, les heures, les
-minutes et les secondes séparant la date d’aujourd’hui de celle de la
-nouvelle année.
-L’objet Date en javascript contient le nombre de millisecondes écoulés
-depuis le 1er janvier 1970.
-Dans la fonction « compteur », créer deux variables « newYearDate » et
-« dateCourante » valant respectivement new Date(newyear) et new Date
-().
-Grâce à la fonction console.log, il est possible d’afficher dans la console
-du navigateur (touche F12) le contenu d’une variable. Tester ceci avec
-console.log (newYearDate – dateCourante).
-4. Le résultat de newYearDate – dateCourante est en milliseconde, convertir
-ce résultat en secondes, puis l’affecter à une variable « totalSecondes ».
-5. Calculer le nombre de jours, d’heures, de minutes et de secondes
-contenues dans la variable « totalSecondes ». Ces résultats, approchés par
-défaut, seront affectées à autant de variables qu’il y a de calculs.
-6. Afin de pouvoir interagir avec la page html, il va falloir afficher les résultats
-de la question précédente dans les conteneurs prévus à cet effet.
 
-Chaque conteneur possède un identifiant (id) dans le code html, et la
-méthode getElementbyId() permet d’associer le script avec ces
-conteneurs.
-Avant la fonction, créer une variable « jourElt » affecté de la valeur de
-document.getElementbyId("jours").
-Faire de même avec des variables « heureElt », « minuteElt » et
-« secondeElt » en les associant aux bonnes balises.
-7. Dans la fonction « compteur », pour afficher les valeurs des jours, heures,
-minutes et secondes, nous allons utiliser la méthode innerHTML sur les
-variables « jourElt », « heureElt » ...
-Ainsi l’instruction : jourElt.innerHTML = jours affichera sur la page html
-dans le conteneur dont l’id est « jour » le contenu de la variable « jours ».
-Faire de même pour les trois autres conteneurs.
-8. A la fin du code, ajouter l’appel de la fonction « compteur » ainsi qu’un
-appel toutes les secondes de cette même fonction grâce à setInterval.
-*/
 
 let newyear = "1 jan 2025"; // Date de la nouvelle année au format "jour mois année"
 let jourElt = document.getElementById("jours");
@@ -46,27 +6,58 @@ let heureElt = document.getElementById("heures");
 let minuteElt = document.getElementById("minutes");
 let secondeElt = document.getElementById("secondes");
 
+
 function compteur() {
 
-  let newYearDate = new Date(newyear); // Conversion de la chaîne en objet Date
-  let dateCourante = new Date(); // Date actuelle
-  console.log(newYearDate - dateCourante);
 
-  let totalSecondes = (newYearDate - dateCourante) / 1000
-  let jours = Math.floor(totalSecondes / (3600 * 24))
-  let heures = Math.floor(totalSecondes % (3600 * 24))
-  let minutes = Math.floor((totalSecondes % 3600) / 60)
-  let secondes = Math.floor(totalSecondes % 60)
-  console.log(jours, heures, minutes, secondes)
+    let newYearDate = new Date(newyear);
+    let dateCourante = new Date();
+    console.log(newYearDate - dateCourante);
 
-  jourElt.innerHTML = jours
-  heureElt.innerHTML = heures
-  minuteElt.innerHTML = minutes
-  secondeElt.innerHTML = totalSecondes
+
+    let totalSecondes = (newYearDate - dateCourante) / 1000
+    let jours = Math.floor(totalSecondes / (3600 * 24))
+    let heures = Math.floor(totalSecondes % (3600 * 24)/3600)
+    let minutes = Math.floor((totalSecondes % 3600) % 3600 / 60)
+    let secondes = Math.floor((totalSecondes % 3600) % 3600 % 60)
+    console.log(jours, heures, minutes, secondes)
+
+
+    jourElt.innerHTML = jours
+    heureElt.innerHTML = heures
+    minuteElt.innerHTML = minutes
+    secondeElt.innerHTML = secondes
+    console.log("pipou")
+
+    if (secondes < 10) {
+        secondeElt.innerHTML = "0" + secondes
+    } else {
+        secondeElt.innerHTML = secondes
+    }
+
+    if (minutes < 10) {
+        minuteElt.innerHTML = "0" + minutes
+    } else {
+        minuteElt.innerHTML = minutes
+    }
+
+    if (heures < 10) {
+        heureElt.innerHTML = "0" + heures
+    } else {
+        heureElt.innerHTML = heures
+    }
+
+    if (jours < 10) {
+        jourElt.innerHTML = "0" + jours
+    } else {
+        jourElt.innerHTML = jours
+    }
+
+
 }
 
+
 // Appel de la fonction pour tester
-compteur();
 
 
 setInterval(compteur, 1000);
